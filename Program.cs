208b820b138091027bcaad797e36d181b6a7c9ab
Chris Taylor <chrismtaylor1998@gmail.com>
2021-06-04 13:45:07 -0400

    deseralized json object

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 using RestSharp;
+using Newtonsoft.Json;
 
 
 namespace boomTownApi
@@ -9,10 +10,25 @@ namespace boomTownApi
     {
         static void Main(string[] args)
         {
+            //1. Output Data
+
+            // Contact api and get top level information
             var client = new RestClient("https://api.github.com/orgs/boomtownroi");
             var request = new RestRequest(Method.GET);
             var response = client.Execute(request).Content;
             Console.WriteLine(response);
+
+            // turn JSON string into a c# object
+            dynamic deserialize = JsonConvert.DeserializeObject(response);
+            Console.WriteLine(deserialize.login);
+
+            //contact 
+
+            // get all urls and test if they return code 200 OK
+
+
+            //2. Perform Verifications
+
         }
     }
 }
