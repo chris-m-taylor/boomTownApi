1bbd9892ddfccaacd7f251c343d3645f23e73233
Chris Taylor <chrismtaylor1998@gmail.com>
2021-06-04 12:55:29 -0400

    init commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using RestSharp;
+
+
+namespace boomTownApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://api.github.com/orgs/boomtownroi");
+            var request = new RestRequest(Method.GET);
+            var response = client.Execute(request).Content;
+            Console.WriteLine(response);
+        }
+    }
+}
